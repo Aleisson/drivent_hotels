cf6ce8c10af58ddf60e44ee27433845d7a076fae
Aleisson <caioaleisson@gmail.com>
2022-11-26 17:38:54 -0300

    feat(hotel.test):add test for authentification

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -1,5 +1,7 @@
 import app, { init } from "@/app";
+import faker from "@faker-js/faker";
 import supertest from "supertest";
+import httpStatus from "http-status";
 
 beforeAll(async () => {
   await init();
@@ -8,9 +10,17 @@ beforeAll(async () => {
 const server = supertest(app);
 
 describe("GET /hotels", () => {
-  it("shoud respond with status 501", async () => {
+  it("should respond with status 401 if no token is given", async () => {
     const response = await server.get("/hotels");
 
-    expect(response.status).toBe(501);
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get("/hotels").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
 });
